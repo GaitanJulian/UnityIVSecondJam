a1e2941858de7cb99b31cfaeab42e81e27b22f62
Julian Gaitan <58521037+GaitanJulian@users.noreply.github.com>
2023-09-28 15:51:59 -0500

    Add interaction UI

--- Assets/Code/Scripts/Character/PlayerInteraction.cs
+++ Assets/Code/Scripts/Character/PlayerInteraction.cs
@@ -5,35 +5,70 @@ public class PlayerInteraction : MonoBehaviour
     public float interactionDistance = 3f; // Adjust this as needed
     private Camera playerCamera; // Reference to the player's camera
 
+    // Define a delegate for interactable state changes
+    public delegate void InteractableStateChanged(bool isInteractable);
+    public event InteractableStateChanged OnInteractableStateChanged;
+
     void Start()
     {
-        // Assuming your player character has a camera as a child, you can find it like this:
         playerCamera = GetComponentInChildren<Camera>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            RaycastHit hit;
-            // Cast a ray from the camera's position in the forward direction
-            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionDistance))
-            {
-                DoorInteraction doorInteraction = hit.collider.GetComponent<DoorInteraction>();
-                if (doorInteraction != null)
-                {
-                    // Call the OpenDoors method of the DoorInteraction script
-                    doorInteraction.OpenDoors();
-                }
+        // Continuously shoot the raycast
+        ShootRaycast();
+    }
 
-                KeyInteraction keyInteraction = hit.collider.GetComponent<KeyInteraction>();
-                if (keyInteraction != null) 
-                {
-                    keyInteraction.Addkey();
-                }
+    void ShootRaycast()
+    {
+        RaycastHit hit;
+        // Cast a ray from the camera's position in the forward direction
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionDistance, LayerMask.GetMask("Interactable")))
+        {
+            HandleInteraction(hit);
+
+            // Notify subscribers (the UIManager) that the interactable state has changed
+            if (OnInteractableStateChanged != null)
+            {
+                OnInteractableStateChanged(true);
+            }
+        }
+        else
+        {
+            // Notify subscribers (the UIManager) that the interactable state has changed
+            if (OnInteractableStateChanged != null)
+            {
+                OnInteractableStateChanged(false);
+            }
+        }
+
+
+    }
+
+    void HandleInteraction(RaycastHit hit)
+    {
+        DoorInteraction doorInteraction = hit.collider.GetComponent<DoorInteraction>();
+        KeyInteraction keyInteraction = hit.collider.GetComponent<KeyInteraction>();
+
+        if (doorInteraction != null)
+        {
+            // Call the OpenDoors method of the DoorInteraction script
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                doorInteraction.OpenDoors();
+            }
+        }
+        else if (keyInteraction != null)
+        {
+            // Call the Addkey method of the KeyInteraction script
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                keyInteraction.Addkey();
             }
         }
     }
+
 
     void OnDrawGizmos()
     {
